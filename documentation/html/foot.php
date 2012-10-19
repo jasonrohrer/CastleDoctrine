93f700cab9305cbdb65a346911c508fcf3323c15
Jason Rohrer <jasonrohrer@fastmail.fm>
2012-10-19 09:50:41 -0700

    Tried to add sitemeter, but it doesn't seem to be working, so added getclicky instead.

--- documentation/html/foot.php
+++ documentation/html/foot.php
@@ -1,4 +1,23 @@
 
+
+<script src="//static.getclicky.com/js" type="text/javascript"></script>
+<script type="text/javascript">try{ clicky.init(100538528); }catch(e){}</script>
+<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/100538528ns.gif" /></p></noscript>
+
+
+
+<!--
+
+Site meter not working currently!
+
+<script type="text/javascript" src="http://s47.sitemeter.com/js/counter.js?site=s52castledoctrinemain">
+</script>
+<noscript>
+<a href="http://s47.sitemeter.com/stats.asp?site=s52castledoctrinemain" target="_top">
+<img src="http://s47.sitemeter.com/meter.asp?site=s52castledoctrinemain" alt="Site Meter" border="0"/></a>
+</noscript>
+-->
+
 </body>
 
 </html>
