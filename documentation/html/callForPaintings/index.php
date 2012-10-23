06dbe5bc518d64b884ba65700a2383fa5e9bf566
Jason Rohrer <jasonrohrer@fastmail.fm>
2012-10-23 14:53:18 -0700

    Working on call for paintings.  Almost done.  Need to proofread.

---
+++ documentation/html/callForPaintings/index.php
@@ -0,0 +1,98 @@
+<html>
+
+<head>
+<title>The Castle Doctrine: A Call for Paintings</title>
+</head>
+
+<body bgcolor="#FFFFFF" text="#000000" link="#B65500" alink="#B65500" vlink="#B65500">
+
+
+<center>
+<table border=0 width=75%><tr>
+<td>
+<br>
+<br>
+<center>
+<font size=6>Paintings for <a href="..">The Castle Doctrine</a></font>
+</center>
+
+<br>
+<br>
+
+This page is intended for you, but only if I sent you a link directly.  I'm not equipped to handle painting submissions from hundreds of people, nor would the game benefit from such an overload.
+
+<br>
+<br>
+
+<h3>Quick intro</h3>
+The Castle Doctrine is a massively multiplayer game where each person maintains and secures a house against robbery, but also goes out into the night to rob the houses of other players.  These two activities form a tight, resource constrained loop:  you only get more money for adding security to your house by successfully robbing the houses of others.
+<br>
+<br>
+The intended aesthetic experience is one of violation, with players themselves motivated to fill the role of the violators.  There's nothing like coming home to find that everything has been taken, especially if your prized, irreplaceable possessions are gone.  And that's where you folks come in.
+
+<h3>Unique objects</h3>
+Along with money and other generic in-game objects, your house can also hold one-of-a-kind pieces of artwork.  Everything is vulnerable to theft if a robber successfully negotiates your home security.  Artwork thus passes from robber to robber, or returns the the public auction house if a home owner dies.  And each piece is indeed unique, with only a single person in the world possessing it at any given moment.
+
+<h3>Your help</h3>
+My aim is to populate the game with paintings from various people that I know.  If you decide to help me out, your artwork will be auctioned, bought, prized, collected, coveted, stolen, re-stolen, reclaimed by the state, and auctioned again.  Over and over, for the effective life of my game.  I hope this sounds like something you'll want to be part of.  Fortunately, you don't have to do much---all I need from you is <b>1024 little pixels</b>.
+
+<h3>Specs</h3>
+All paintings in the game are <b>32 x 32 pixel RGB images</b>.  Save it into a lossless format like PNG (preferred) or TIFF.  I also need a <b>title</b> and a <b>by line</b> from you.  Email it all to me.  That's it.  Make one or many---your choice.<br>
+<br>
+Contents are totally up to you.  Drawings, paintings, scans, photographs, or even words.  Whatever you can fit in a 32 x 32 image.  Here are two samples that I made:
+<center>
+<table border=0 cellspacing=30>
+<tr>
+<td><img src="rohrerOwl.png" width=32 height=32"></td>
+<td><img src="rohrerChicory.png" width=32 height=32"></td>
+</tr>
+</table>
+</center>
+Yeah, they're really tiny!  Here they are blown up so you can see the pixels better:
+<center>
+<table border=0 cellspacing=30>
+<tr>
+<td><img src="rohrerOwl_big.png" width=128 height=128"></td>
+<td><img src="rohrerChicory_big.png" width=128 height=128"></td>
+</tr>
+</table>
+</center>
+
+Your paintings will be blown up somewhat in the game (depending on the player's screen size), with sharp pixels preserved.
+
+<h3>Beginner Tips</h3>
+Some of you may have no experience making pixel art or even digital images in general.  Here are some tips.
+<br>
+<br>
+Free tools include <a href="http://mtpaint.sf.net">mtPaint</a> (Linux and Windows) and <a href="http://www.gimp.org">GIMP</a> (Linux, Windows, and Mac).  You can also use PhotoShop if you have it, or even the Paint program that ships with Windows.<br>
+<br>
+Create a new 32 x 32 image, and zoom into it so that it almost fills the screen.  Pixels will become huge squares at that zoom level, and you can place them one-by-one with the mouse. 
+
+
+
+<h3>Details</h3>
+No transparency, please.  The background that the paintings will be displayed on is pure black, if that matters to you.  Your title and by line together should not be more than 44 letters long.
+
+<br>
+<br>
+<br>
+Thanks for helping me out!
+</td>
+</tr>
+
+<tr>
+<td align=right>
+<br>
+<br>
+<br>
+Jason Rohrer<br>
+Davis, CA<br>
+October 2012
+</td>
+</tr>
+</table>
+
+</body>
+
+
+</html>
