6326d31b1a917060766848f9e9c52d8dcfeb6a86
Jason Rohrer <jasonrohrer@fastmail.fm>
2012-10-23 11:39:25 -0700

    Fixed visited link color.

--- documentation/html/head.php
+++ documentation/html/head.php
@@ -4,6 +4,6 @@
 <title>The Castle Doctrine</title>
 </head>
 
-<body bgcolor="#000000" text="#FFFFFF" link="#FF7700" alink="#CC9614" vlink="#DB6600">
+<body bgcolor="#000000" text="#FFFFFF" link="#FF7700" alink="#CC9614" vlink="#FF7700">
 
 
