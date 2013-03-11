fc3e3a294358873c089a809f1843869deeae77f2
Jason Rohrer <jasonrohrer@fastmail.fm>
2013-03-11 14:30:04 -0700

    Working on buy page for site.

---
+++ documentation/html/buy.php
@@ -0,0 +1,139 @@
+<?php
+
+// no caching
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', FALSE);
+header('Pragma: no-cache');
+
+error_reporting( E_ALL );
+
+
+$pathToRoot = "";
+
+include( "header.php" );
+
+
+function showPayLinks( $inSimple ) {
+    $referrer = "";
+    
+    if( isset( $_SERVER['HTTP_REFERER'] ) ) {
+        // pass it through without a regex filter
+        // because we can't control its safety in the end anyway
+        // (user can just edit URL sent to FastSpring).
+        $referrer = urlencode( $_SERVER['HTTP_REFERER'] );
+        }
+    
+    
+
+    ?>
+ <center>
+      <center><table border=0><tr><td> 
+<ul> 
+      <li>Early access alpha testing
+      <li>Lifetime server account
+      <li>All future updates
+      <li>Tech support included
+      </ul>
+</td></tr></table>
+
+      <font size=5>Available now for $8</font><br>
+      <font size=4 color=#ff0000>(50% off final price during alpha)</font><br><br>
+      
+      <a href="https://sites.fastspring.com/jasonrohrer/instant/starfilledskyticket?referrer=<?php echo $referrer;?>">
+      <img src="fs_cards.png" width=280 height=45 border=0><?php
+      if( !$inSimple ) {
+
+          echo "<br>";
+          
+          echo "<img src=\"fs_button05.png\" width=210 height=60 border=0>";
+          }
+?>
+      </a>
+      </center>
+<?php
+    }
+
+function showLogo( $inImageFile, $inText ) {
+
+    echo "<table border=0><tr><td align=center>
+          <img src=\"$inImageFile\"><br>
+          <font size=1>$inText</font>
+          </td></tr></table>";
+    }
+
+?>
+
+
+
+<center>
+
+<font size=6>The Castle Doctrine</font><br>
+a massively-multiplayer game of burglary and home defense by
+<a href="http://hcsoftware.sf.net/jason-rohrer">Jason Rohrer</a>
+<br>
+<br>
+<br>
+
+<font size=5>
+"A powerful, memorable game."</font> --Alex Meer, RPS [<a href="http://www.rockpapershotgun.com/2013/03/07/castle-doctrine-preview-2/">1</a>] [<a href="http://www.rockpapershotgun.com/2013/03/08/the-castle-doctrine/">2</a>] 
+<br>
+<br>
+
+<br>
+<br>
+<table border=0 width=75% cellpadding=5><tr><td bgcolor="#222222">
+<font size=5>It's 1991, and things are bad.</font> You're a guy with a wife, two kids, and a house.... [<a href="details.php">more</a>]</td></tr></table>
+<br>
+<br>
+
+<center>
+<table border=0><tr>
+<td><?php showLogo( "noDRM.png", "No DRM" ); ?></td>
+<td><?php showLogo( "noTie.png", "No middle-person" ); ?></td>
+<td><?php showLogo( "crossPlatform.png", "Cross-platform" ); ?></td>
+<td><?php showLogo( "openSource.png", "Open Source" ); ?></td>
+</tr></table>                                  
+</center>
+
+<center>
+<table border=0 cellpadding=2><tr><td bgcolor="#222222">
+<table border=0 cellpadding=5><tr><td bgcolor="#000000">
+<center> 
+
+<?php
+   showPayLinks( false );
+?>
+
+</td></tr></table>
+</td></tr></table>
+</center>
+<br>
+<br>
+</center>
+
+<font size=5>What you get</font><br>
+
+<table border=0 width="100%" cellpadding=5><tr><td bgcolor="#222222">
+Immediately after your payment is processed, you will receive an email with an access link.  You will then be able to download all of the following DRM-free distributions:
+<center>
+<table border=0><tr><td>
+<ul>
+<li>Windows build</li>
+<li>MacOS build (10.5 and later Intel)</li>
+<li>Full source code bundle (which can also be compiled on GNU/Linux)</li>
+</ul>
+</td></tr></table>
+</center>
+The price also includes downloads of all future updates and a lifetime account on the main game server that I am running.<br>
+<br>
+The source bundle includes the server software, allowing you to <b>set up and run your own server</b> (a web server with PHP/MySQL is required).<br>
+<br>
+You can take a look at the <a href="requirements.php">system requirements</a>.
+</td></tr></table>
+<br>
+<br>
+
+
+
+
+<?php include( "footer.php" ); ?>
---
+++ documentation/html/details.php
@@ -0,0 +1,31 @@
+<?php
+
+// no caching
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', FALSE);
+header('Pragma: no-cache');
+
+error_reporting( E_ALL );
+
+
+$pathToRoot = "";
+
+include( "header.php" );
+
+?>
+
+<center>
+<br>
+<br>
+<table border=0 width=75% cellpadding=5><tr><td bgcolor="#222222">
+<font size=5>It's 1991, and things are bad.</font> You're a guy with a wife, two kids, and a house.  You're nearly broke, but you know that they'll be coming soon for what you've got left, hundreds of them. You must spend what you have to secure what's yours.  But every wall can be cut through, and every window can be broken, so you've got to get smarter than that. With doors, wires, switches, and dogs, building something perplexing is easy. Building something perplexing that still lets you get in and your family get out is much harder. After all, deadly traps aren't just deadly to intruders.  Death through carelessness is quick and severe.<br>
+<br>
+
+But who are these intruders, and where are they coming from? One night, after your grand security plans are choked back by your dwindling budget, you head out for a walk. As you pass other people's houses, you notice that many people aren't home, and some of them clearly don't suffer from dwindling budgets. You climb the front steps of one house, knock softly, and find that the front door is unlocked. The gears in your mind start turning. No harm in having a quick look around...
+</td></tr></table>
+<br>
+<br>
+</center>
+
+</center>
+<?php include( "footer.php" ); ?>
