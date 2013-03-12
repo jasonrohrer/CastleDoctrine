5d4d52fb1cc47e1d6b236030c736cf6ad382e5b6
Jason Rohrer <jasonrohrer@fastmail.fm>
2013-03-11 22:25:01 -0700

    Working on buy page for site.

---
+++ documentation/html/requirements.php
@@ -0,0 +1,21 @@
+<?php
+$pathToRoot = "";
+include( "header.php" ); ?>
+
+<font size=6>System Requirements</font><br>
+<table border=0 width="100%" cellpadding=5><tr><td bgcolor="#222222">
+The game was developed on a 900MHz laptop where it runs at 60 frames per second and is perfectly playable.  In other words, it will most likely run on your computer.<br>
+<br>
+The only required library is OpenGL, which ships standard on MacOS and Windows and is readily available on GNU/Linux as well.<br>
+<br>
+Other stuff you will need:
+<ul>
+<li>keyboard</li>
+<li>mouse</li>
+<li>speakers or headphones</li>
+</ul>
+</td></tr></table>
+<br>
+<br>
+
+<?php include( "footer.php" ); ?>
