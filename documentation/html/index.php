bcec94505c81338c008be0443d35b9d546dffacf
Jason Rohrer <jasonrohrer@fastmail.fm>
2013-03-12 10:13:33 -0700

    Changed index to point to buy page.

--- documentation/html/index.php
+++ documentation/html/index.php
@@ -3,11 +3,11 @@
 <center>
 
 
-<a href="about.php"><image border=0 width=532 height=263 src="alarm.jpg"></a>
+<a href="buy.php"><image border=0 width=532 height=263 src="alarm.jpg"></a>
 
 <br>
 
-<a href="about.php"><image border=0 width=228 height=279 src="listing.jpg"></a>
+<a href="buy.php"><image border=0 width=228 height=279 src="listing.jpg"></a>
 
 </center>
 
