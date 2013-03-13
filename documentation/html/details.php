27887253f40095049709304441953858a0eaf82a
Jason Rohrer <jasonrohrer@fastmail.fm>
2013-03-12 18:35:13 -0700

    Fixed alignment.

--- documentation/html/details.php
+++ documentation/html/details.php
@@ -18,7 +18,7 @@ include( "header.php" );
 <br>
 <br>
 <table border=0 width=75% cellpadding=5><tr><td bgcolor="#222222">
-<table border=0 cellpadding=5><tr><td><img src="detailImages/family.png" width=168 height=154 border=0></td><td valign=top>
+<table border=0 cellpadding=5><tr><td valign=top><img src="detailImages/family.png" width=168 height=154 border=0></td><td valign=top>
 <font size=5>It's 1991, and things are bad.</font> You're a guy with a wife, two kids, and a house.  You're nearly broke, but you know that they'll be coming soon for what you've got left, hundreds of them.
 </td></tr></table>
 </td></tr></table>
@@ -29,7 +29,7 @@ include( "header.php" );
 <table border=0 width=75% cellpadding=5><tr><td bgcolor="#222222">
 <table border=0 cellpadding=5><tr>
 <td valign=top>You must spend what you have to secure what's yours.  But every wall can be cut through, and every window can be broken, so you'll have to be smarter than that. With doors, wires, switches, and dogs---building something perplexing is easy.</td>
-<td><img src="detailImages/unfairHouse.png" width=168 height=154 border=0></td>
+<td valign=top><img src="detailImages/unfairHouse.png" width=168 height=154 border=0></td>
 </tr></table>
 </td></tr></table>
 
@@ -38,7 +38,7 @@ include( "header.php" );
 <br>
 <table border=0 width=75% cellpadding=5><tr><td bgcolor="#222222">
 <table border=0 cellpadding=5><tr>
-<td><img src="detailImages/deadInOwnHouse.png" width=168 height=154 border=0></td>
+<td valign=top><img src="detailImages/deadInOwnHouse.png" width=168 height=154 border=0></td>
 <td valign=top>Building something perplexing that still lets you get in and your family get out is much harder. After all, deadly traps aren't just deadly to intruders.  Death through carelessness is quick and severe.</td>
 </tr></table>
 </td></tr></table>
@@ -49,7 +49,7 @@ include( "header.php" );
 <table border=0 width=75% cellpadding=5><tr><td bgcolor="#222222">
 <table border=0 cellpadding=5><tr>
 <td valign=top>But who are these intruders, and where are they coming from? One night, after your grand security plans are choked back by your dwindling budget, you head out for a walk. As you pass other people's houses, you notice that many people aren't home, and some of them clearly don't suffer from dwindling budgets. </td>
-<td><img src="detailImages/houseList.png" width=168 height=154 border=0></td>
+<td valign=top><img src="detailImages/houseList.png" width=168 height=154 border=0></td>
 </tr></table>
 </td></tr></table>
 
@@ -58,7 +58,7 @@ include( "header.php" );
 <br>
 <table border=0 width=75% cellpadding=5><tr><td bgcolor="#222222">
 <table border=0 cellpadding=5><tr>
-<td><img src="detailImages/otherHouse.png" width=168 height=154 border=0></td>
+<td valign=top><img src="detailImages/otherHouse.png" width=168 height=154 border=0></td>
 <td valign=top>You climb the front steps of one house, knock softly, and find that the front door is unlocked. The gears in your mind start turning. No harm in having a quick look around.  You open the door...</td>
 </tr></table>
 </td></tr></table>
