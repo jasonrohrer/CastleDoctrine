ac415ed3e55bbe355ba21c968fab2406caededf6
Jason Rohrer <jasonrohrer@fastmail.fm>
2013-05-31 06:56:47 -0700

    Increased salary for un-broken houses by a factor of 10.

--- server/settings.php
+++ server/settings.php
@@ -379,8 +379,8 @@ $playerStartMoney = 2000;
 // how often absent (house not checked out for edit) player and wife get paid
 $payInterval = "0 1:00:0.000";
 
-$playerPayAmount = 14;
-$wifePayAmount = 28;
+$playerPayAmount = 140;
+$wifePayAmount = 280;
 
 
 
