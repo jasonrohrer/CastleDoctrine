2164f183055055c8314aeb771a104c19314e5da4
Jason Rohrer <jasonrohrer@fastmail.fm>
2017-11-15 08:02:38 -0800

    Added discord widget to homepage.

--- documentation/html/buy.php
+++ documentation/html/buy.php
@@ -166,6 +166,13 @@ You can take a look at the <a href="requirements.php">system requirements</a>.
 <br>
 <br>
 
+<center><iframe src="https://discordapp.com/widget?id=267002994426511360&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0"></iframe></center>
+
+<br>
+<br>
+<br>
+
+    
 [<a href="credits.php">Game Credits</a>]
 
 
