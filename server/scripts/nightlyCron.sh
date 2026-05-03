e7cd1609dd4d0553a51577a4ca1df4048754b43e
Jason Rohrer <jasonrohrer@fastmail.fm>
2026-05-03 12:41:45 -0400

    Messages for debugging when client down.

--- server/scripts/nightlyCron.sh
+++ server/scripts/nightlyCron.sh
@@ -14,14 +14,17 @@ responseB=$(echo -n "check_alive\n[END_REQUEST]" | netcat localhost $portB)
 clientDown=0
 
 if [[ "$responseA" != *OK* ]]; then
+	echo "Headless client on port $portA down"
 	clientDown=1
 fi
 
 if [[ "$responseB" != *OK* ]]; then
+	echo "Headless client on port $portB down"
 	clientDown=1
 fi
 
 
 if [[ $clientDown -eq 1 ]]; then
+	echo "At least one client down, killing and rebuilding both."
 	/home/jcr13/buildAndLaunchHeadlessClients.sh
 fi
